b3de2328417984da4e31da87216b91bbbfb70659
KashifAhmad1789 <Kash766.ka@gmail.com>
2022-05-09 10:48:38 +0530

    two quuestion

--- Array/Longpressedname.cpp
+++ Array/Longpressedname.cpp
@@ -14,10 +14,61 @@ struct TreeNode
 /********************************** Boiler Plate End  ***********************************************************/
 // Questi ~on-- > https : // leetcode.com/problems/long-pressed-name/
 //  Main code Start Here
-
-void solve(int arr[], int n){
-    // CODE HERE
-
+class Solution2
+{
+public:
+    bool isLongPressedName(string name, string typed)
+    {
+        if (name[0] != typed[0])
+            return false;
+        int i = 0, j = 0;
+        while (i < typed.size() || j < name.size())
+        {
+            if (typed[i] == name[j])
+                j++;
+            else if (typed[i] != typed[i - 1])
+                return false;
+            i++;
+        }
+        return true;
+    }
+};
+class solution
+{
+public:
+    bool isLongpressedName(string name, string typed)
+    {
+        int n = name.size();
+        int m = typed.size();
+        int i = 0, j = 0;
+        if (n > m)
+            return false;
+        while (i < n)
+        {
+            if (name[i] == typed[j])
+            {
+                i++;
+                j++;
+            }
+            else if (j != 0 && typed[j] == typed[j - 1])
+            {
+                j++;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        while (j < m)
+        {
+            if (typed[j] != name[n - 1])
+            {
+                return false;
+            }
+            j++;
+        }
+        return true;
+    }
 };
 int main()
 {
